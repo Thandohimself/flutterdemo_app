9851c12c83393f59af4dbe30a5d7648e800c154d
Thandohimself <thandohimself@gmail.com>
2019-11-26 23:01:08 +0200

    Stateless Widgets & Hot Reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('My first App'),
-      centerTitle: true,
-      backgroundColor: Colors.green[600],
-    ),
-    body: Center(
-      child: Text(
-          'Hello Thandos',
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                  letterSpacing: 2.0,
-                color: Colors.amber[600],
-                fontFamily: 'GoogleRoboto',
-
-              ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text('Click'),
-      backgroundColor: Colors.green[600],
-    ),
-  ),
+  home: Home(),
 ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My first App'),
+        centerTitle: true,
+        backgroundColor: Colors.green[600],
+      ),
+      body: Center(
+        child: Text(
+          'Hello Thandos',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.amber[600],
+            fontFamily: 'GoogleRoboto',
+
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click '),
+        backgroundColor: Colors.green[600],
+      ),
+    );
+  }
+}
